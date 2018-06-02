54e1f05ae6b7f9173aaa28a8348f3ef4002915ca
thomasMilesTgm <thomas.miles.tgm@gmail.com>
2018-06-03 06:43:09 +1000

    draft

--- Workshop4/Wk4.m
+++ Workshop4/Wk4.m
@@ -1,4 +1,4 @@
-close all
+
 hold on
 syms s;
 g = 9.81;
@@ -25,51 +25,68 @@ T2 = 0.0001;
 
 
 k = 10.5;
-me = 1*tf([1 5], [0.001 1 100])
+me = 1*tf([1 5], [0.001 1 100]);
 pid = k*tf([1.252, 38.58, 100],[0.003 1 0]);
 plant = tf(NUM,DEN);
 ol = pid*plant;
+%rlocus(plant);
+%title('plant root locus');
+%impulse (me*plant/(1+me*plant))
 
-impulse (me*plant/(1+me*plant))
+%figure
+%
+%title("plant")
 
-figure
-bode(me*plant)
-
-title("me plant")
-
-K = 50;
+K = 100;
 % LEAD2 CONTROLLER
-z = 0.2;
+z = 0.1;
 p = 0.001;
 
 lead = tf([z 1], [p 1])
 
-figure
-rlocus(lead)
+%figure
+%rlocus(lead)
 
-figure
-bode(lead)
-title("lead controll")
+%figure
+%bode(lead)
+%title("lead controll")
 
 
 % repeat the pole to allow a gain margin to exist.
-p2 = 0.1;
-repeat = tf(1, [p2 1]);
+p2 = 2.001;
+z2 = 0.4;
+repeat = tf([1], [p2 1]);
 
 
 
 sys = lead*plant
-figure
-bode(K*sys/(1+K*sys))
-title("closedloop")
 
-figure
-rlocus(sys)
+%figure
+%bode(K*sys/(1+K*sys))
+%title("closedloop")
+T = K*sys/(1+K*sys)
+
+
+%rlocus(K*sys)
 
 [Gm,Pm,Wgm,Wpm] = margin(K*sys);
+
+
+%bode (T)
+%title("$T_0$", 'interpreter','latex')
 
 phase_m = Pm
 gain_m = Gm
 
-figure
-step(K*sys/(1+K*sys))
+%legend('K = 25', 'K = 50', 'K = 100')
+%figure
+
+opt = stepDataOptions('StepAmplitude', -1);
+impulse(feedback(K*sys, 1), opt);
+
+
+
+
+
+
+
